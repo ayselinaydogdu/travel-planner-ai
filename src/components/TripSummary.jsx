63d2e274810e0b5b7e31808aa332737ebbb37a24
ayselinaydogdu@posta.mu.edu.tr <ayselinaydogdu@posta.mu.edu.tr>
2026-07-10 17:19:47 +0300

    son düzenleme

--- src/components/TripSummary.jsx
+++ src/components/TripSummary.jsx
@@ -2,36 +2,30 @@ function TripSummary({ trip }) {
   return (
     <div className="preview">
       <h3>🌍 Your Trip Summary</h3>
-
       <div className="trip-info">
         <div className="info-card">
           <div className="info-title">📍 Departure</div>
           <div className="info-value">{trip.from}</div>
         </div>
-
         <div className="info-card">
           <div className="info-title">✈️ Destination</div>
           <div className="info-value">{trip.to}</div>
         </div>
-
         <div className="info-card">
           <div className="info-title">📅 Duration</div>
           <div className="info-value">{trip.days} Days</div>
         </div>
-
         <div className="info-card">
           <div className="info-title">💶 Budget</div>
           <div className="info-value">€{trip.budget}</div>
         </div>
-
         <div className="info-card">
           <div className="info-title">✈️ Style</div>
-          <div className="info-value">{trip.travelStyle}</div>
+          <div className="info-value">{trip.travelStyle.join(", ")}</div>
         </div>
-
         <div className="info-card">
           <div className="info-title">❤️ Interest</div>
-          <div className="info-value">{trip.interest}</div>
+          <div className="info-value">{trip.interest.join(", ")}</div>
         </div>
       </div>
     </div>
